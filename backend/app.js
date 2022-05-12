9b05181540b1bf3d6f1cba40c5307f9735e35a68
Afriad1992 <ghizlane.afriad@gmail.com>
2022-05-12 17:05:11 +0200

    projet

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+require('./database');
+const path = require('path');
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+  app.use(express.urlencoded({ extended : false }));
+  app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.urlencoded({ extended : false }));
+app.use(express.json());
+const user=require('./routes/user.routes');
+const sauce=require('./routes/sauce.routes');
+app.use(express.urlencoded({ extended : false }));
+app.use(express.json());
+app.use("/api/auth", user);
+app.use("/api/sauces", sauce);
+
+module.exports = app;
